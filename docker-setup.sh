e766b1092cab97a402246e728dd7e3890a38c2b9
D Raymond <raymondd@vt.edu>
2022-09-05 20:37:05 -0400

    update to docker setup script

--- docker-setup.sh
+++ docker-setup.sh
@@ -3,6 +3,6 @@ sudo apt -y install apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt -y install docker-ce
 groupadd docker
 sudo usermod -aG docker draymond
